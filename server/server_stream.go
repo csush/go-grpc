cdf2fe473290571b306c43c8ad457c2e65352c4f
Sushant Chaudhary <csush.95@gmail.com>
2025-07-08 12:45:39 +0200

    feat: added server side streaming

---
+++ server/server_stream.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"time"
+
+	pb "github.com/csush/go-grpc/proto"
+
+	"log"
+)
+
+func (s *helloServer) SayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
+	log.Printf("Got request with names: %v", req.Names)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello " + name,
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return nil
+}
